2225b10fe80009333b8df54bbd851fab0ea4223c
antonK2234 <antonK2234@email.com>
2021-11-17 14:59:57 +0200

    commit 1

--- jenkins_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-cd /home/anton/Загрузки/
-sudo dpkg -i jenkins_2.303.2_all.deb
-sudo apt update
-sudo apt install jenkins
